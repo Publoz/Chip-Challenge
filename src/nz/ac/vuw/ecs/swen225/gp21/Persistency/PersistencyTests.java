ce16c3ffdc3e8063cca8affafbef8e3d98026a5f
Phil Edie <ediephil@gmail.com>
2021-09-13 10:50:05 +1200

    Created skeleton for XMLSaveLoad class and basic level 1 XML.

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/Persistency/PersistencyTests.java
@@ -0,0 +1,5 @@
+package nz.ac.vuw.ecs.swen225.gp21.Persistency;
+
+public class PersistencyTests {
+
+}
---
+++ src/nz/ac/vuw/ecs/swen225/gp21/Persistency/XMLSaveLoad.java
@@ -0,0 +1,18 @@
+package nz.ac.vuw.ecs.swen225.gp21.Persistency;
+
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.Game;
+import java.util.ServiceLoader;
+import java.net.URLClassLoader;
+
+public class XMLSaveLoad {	
+	
+	
+	public static Game load() {
+		
+	}
+	
+	public static void save(Game g) {
+		
+	}
+}
