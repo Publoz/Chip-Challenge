ea7c7cdfec81387e4fd7778ab4a2fa381664ec60
Phil Edie <ediephil@gmail.com>
2021-09-16 10:13:14 +1200

    Loads the root node using a DOM parser.
    Tile nodes currently aren't being loaded.

--- src/nz/ac/vuw/ecs/swen225/gp21/Persistency/XMLSaveLoad.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/Persistency/XMLSaveLoad.java
@@ -1,78 +1,121 @@
 package nz.ac.vuw.ecs.swen225.gp21.Persistency;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
-import javax.xml.parsers.SAXParser;
-import java.beans.XMLEncoder;
-import java.beans.XMLDecoder;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import nz.ac.vuw.ecs.swen225.gp21.domain.Free;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Game;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Tile;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
+public class XMLSaveLoad {
 
-public class XMLSaveLoad {	
+	public static final String LEVEL_ONE = "./src/nz/ac/vuw/ecs/swen225/gp21/Persistency/levels/level1.xml";
 
 	/**
-	 * Opens an XML file, converts it to a new Game object, and returns it. 
+	 * Opens an XML file, converts it to a new Game object, and returns it.
 	 * 
-	 * With help from YouTube:
-	 * https://www.youtube.com/watch?v=H-aTpt4NG-s
-	 * @param level  Identifies which level to be loaded by the load method. 
-	 * @return	loaded level. 
+	 * With help from YouTube: https://www.youtube.com/watch?v=H-aTpt4NG-s
+	 * 
+	 * @param level Identifies which level to be loaded by the load method.
+	 * @return loaded level.
+	 * 
 	 */
-	
 	public static Game load(int level) {
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
 		try {
-			FileInputStream fis = new FileInputStream(new File("./src/nz/ac/vuw/ecs/swen225/gp21/Persistency/levels/level" + level + ".xml"));
-			XMLDecoder decoder = new XMLDecoder(fis);
-			Game loaded = (Game) decoder.readObject();
-			decoder.close();
-			fis.close();
+
+			DocumentBuilder db = dbf.newDocumentBuilder();
+
+			Document doc = db.parse(new File(LEVEL_ONE));
+
+			doc.getDocumentElement().normalize();
+
+			System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
+			System.out.println("-----------");
+
+			Element gameNode = (Element) doc.getElementsByTagName("Game").item(0);
+
+			int levelNumber = Integer.parseInt(gameNode.getAttribute("levelNumber"));
+			int totalTime = Integer.parseInt(gameNode.getAttribute("totalTime"));
+			int row = Integer.parseInt(gameNode.getAttribute("row"));
+			int col = Integer.parseInt(gameNode.getAttribute("col"));
+
+			List<Tile> tileList = new ArrayList<Tile>();
+
+			Element tilesNode = (Element) gameNode.getElementsByTagName("Tiles").item(0);
+			System.out.println("TilesNode: " + tilesNode);
+			NodeList allTiles = tilesNode.getChildNodes();
+			System.out.println(allTiles.getLength());
 			
-			System.out.println(loaded.toString());
-			return loaded;
-		} catch (IOException e) {
+			for(int x = 0; x < allTiles.getLength(); x++) {
+				
+				Element column =  allTiles.getElementsByTagName("Items").item(x);
+				
+				for(int y = 0; y < column.item(y)) {
+					
+				}
+				
+
+			}
+			
+			for (int i = 0; i < allTiles.getLength(); i++) {
+				Node node = allTiles.item(i);
+
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) node;
+
+					if (element.getNodeName().equals("Free")) {
+						String key = element.getAttribute("key");
+						boolean treasure = Boolean.parseBoolean(element.getAttribute("treasure"));
+						tileList.add(new Free(key, treasure));
+					}
+				}
+			}
+
+		} catch (ParserConfigurationException | SAXException | IOException e) {
 			e.printStackTrace();
-			return null;
 		}
+		return null;
+	}
+	
+	
+	
+
+	/**
+	 * Saves the given game object to an XML file.
+	 * 
+	 * With help from YouTube: https://www.youtube.com/watch?v=H-aTpt4NG-s
+	 */
+	public static void save(Game game) {
 
 	}
-	
-	/**
-	 * Saves the given game object to an XML file. 
-	 * 
-	 * With help from YouTube:
-	 * https://www.youtube.com/watch?v=H-aTpt4NG-s
-	 */
-	public static void save(Game game) {		
-		try {
-			FileOutputStream fos = new FileOutputStream(new File("./src/nz/ac/vuw/ecs/swen225/gp21/Persistency/levels/saved.xml"));
-			XMLEncoder encoder = new XMLEncoder(fos);
-			encoder.writeObject(game);
-			encoder.close();
-			fos.close();
-			
-		} catch(IOException e) {
-			System.out.println("Oopsie!");
-			e.printStackTrace();
-		}
-	}
-	
-	
+
 	public static void main(String args[]) {
-		Tile[][] board = new Tile[2][2];
-		board[0][0] = new Free("", false);
-		board[1][0] = new Free("", false);
-		board[0][1] = new Free("", false);
-		board[1][1] = new Free("", false);
-		
-		
-		Game g = new Game(board, 60, 1, 1, 1);
-		save(g);
+		/**
+		 * Tile[][] board = new Tile[2][2]; board[0][0] = new Free("", false);
+		 * board[1][0] = new Free("", false); board[0][1] = new Free("", false);
+		 * board[1][1] = new Free("", false);
+		 * 
+		 * Game g = new Game(board, 60, 1, 1, 1); save(g);
+		 */
+
+		load(1);
 	}
-	
+
 }
