69cf4bea9d99968affeb35ae7d8d46da42d7e3c2
Phil Edie <ediephil@gmail.com>
2021-09-17 19:56:04 +1200

    Save/Load now recognizes all tiles.
    Created additional test cases. Made XML schema clearer.

--- src/nz/ac/vuw/ecs/swen225/gp21/Persistency/PersistencyTests.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/Persistency/PersistencyTests.java
@@ -1,47 +1,139 @@
 package nz.ac.vuw.ecs.swen225.gp21.Persistency;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
+import nz.ac.vuw.ecs.swen225.gp21.domain.Door;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Exit;
+import nz.ac.vuw.ecs.swen225.gp21.domain.ExitLock;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Free;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Game;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Info;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Tile;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Wall;
 
 public class PersistencyTests {
+
+	@Test
+	/**
+	 * Testing basic room with no objects. Ensure Game objects can be repeatedly
+	 * saved and loaded without error.
+	 */
+	public void test1() {
+		
+		try {
+			Tile[][] maze = new Tile[5][5];
+			for (int row = 0; row < 5; row++) {
+				for (int col = 0; col < 5; col++) {
+					if (row == 0 || col == 0 || row == maze.length - 1 || col == maze.length - 1) {
+						maze[row][col] = new Wall();
+					} else {
+						maze[row][col] = new Free();
+					}
+				}
+			}
+
+			Game original = new Game(maze, 1, 60, 2, 2);
+			String originalString = original.drawBoard();
+
+			XMLSaveLoad.save(original, "saved.xml");
+			Game loaded = XMLSaveLoad.load("saved.xml");
+			
+
+			for (int i = 0; i < 5; i++) {
+				loaded = XMLSaveLoad.load("saved.xml");
+				XMLSaveLoad.save(loaded, "saved.xml");
+			}
+
+			String loadedString = loaded.drawBoard();
+			
+			assert (originalString.equals(loadedString));
+		} catch (IOException e) {
+			fail();
+		}
+	}
+
+	@Test
+	/**
+	 * Testing to ensure Game objects can be repeatedly saved and loaded without
+	 * error.
+	 */
+	public void test2() {
+
+		try {
+			Tile[][] maze = new Tile[7][7];
+			for (int row = 0; row < 7; row++) {
+				for (int col = 0; col < 7; col++) {
+					if (row == 0 || col == 0 || row == maze.length - 1 || col == maze.length - 1) {
+						maze[row][col] = new Wall();
+					} else {
+						maze[row][col] = new Free();
+					}
+				}
+			}
+
+			maze[1][1] = new Door("#");
+			maze[2][1] = new Exit();
+			maze[3][1] = new ExitLock();
+			maze[4][1] = new Info("Some information");
+			maze[5][1] = new Free("#", false);
+			maze[6][1] = new Free("", true);
+
+			Game original = new Game(maze, 1, 60, 2, 2);
+			String originalString = original.drawBoard();
+
+			XMLSaveLoad.save(original, "saved.xml");
+			Game loaded = XMLSaveLoad.load("saved.xml");
+
+			for (int i = 0; i < 5; i++) {
+				loaded = XMLSaveLoad.load("saved.xml");
+				XMLSaveLoad.save(loaded, "saved.xml");
+			}
+
+			String loadedString = loaded.drawBoard();
+			assert (originalString.equals(loadedString));
+
+		} catch (IOException e) {
+			fail();
+		}
+	}
 	
 	@Test
 	/**
-	 * Testing to ensure Game objects can be repeatedly saved and loaded without error.
+	 * Ensuring only files with extension .xml can be loaded.
 	 */
-	public void test1() {
-		Tile[][] maze = new Tile[5][5];
-		for(int row = 0; row < 5; row++) {
-			for(int col = 0; col < 5; col++) {
-				if(row == 0 || col == 0 || row == maze.length-1 || col == maze.length-1) {
-					maze[row][col] = new Wall();
-				} else {
-					maze[row][col] = new Free();
+	public void test3() {
+		try {
+			XMLSaveLoad.load("level1");
+			fail();
+		} catch(IOException e) {}
+	}
+	
+	@Test
+	/**
+	 * Ensuring files can only be saved to an xml format.
+	 */
+	public void test4() {
+		try {
+			Tile[][] maze = new Tile[5][5];
+			for (int row = 0; row < 5; row++) {
+				for (int col = 0; col < 5; col++) {
+					if (row == 0 || col == 0 || row == maze.length - 1 || col == maze.length - 1) {
+						maze[row][col] = new Wall();
+					} else {
+						maze[row][col] = new Free();
+					}
 				}
 			}
-		}
-		
 
-		Game original = new Game(maze, 1, 60, 2, 2);
-		String originalString = original.drawBoard();
-		
-		XMLSaveLoad.save(original, "saved.xml");
-		Game loaded = XMLSaveLoad.load("saved.xml");
-		System.out.println(originalString);
-		
-		for(int i = 0; i < 5; i++) {
-			loaded = XMLSaveLoad.load("saved.xml");
-			XMLSaveLoad.save(loaded, "saved.xml");
-		}
-
-		String loadedString = loaded.drawBoard();
-		System.out.println(loadedString);
-		assert(originalString.equals(loadedString));
+			Game game = new Game(maze, 1, 60, 2, 2);
+			
+			XMLSaveLoad.save(game, "level1");
+			fail();
+		} catch(IOException e) {}
 	}
-
 }
--- src/nz/ac/vuw/ecs/swen225/gp21/Persistency/XMLSaveLoad.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/Persistency/XMLSaveLoad.java
@@ -8,8 +8,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import nz.ac.vuw.ecs.swen225.gp21.domain.Door;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Exit;
+import nz.ac.vuw.ecs.swen225.gp21.domain.ExitLock;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Free;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Game;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Info;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Tile;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Wall;
 
@@ -30,44 +33,54 @@ public class XMLSaveLoad {
 	 * Parses an XML file with the given filename and returns it as a new Game
 	 * object.
 	 * 
+	 * Help from https://mkyong.com/java/how-to-read-xml-file-in-java-jdom-example/
+	 * 
 	 * @param fileName In the format "name_of_file.xml"
 	 * @return The constructed game object.
+	 * @throws IOException If input file name is in the incorrect format.
 	 */
-	// Help from https://mkyong.com/java/how-to-read-xml-file-in-java-jdom-example/
-	public static Game load(String fileName) {
+	public static Game load(String fileName) throws IOException {
+
+		if (!fileName.endsWith(".xml")) {
+			throw new IOException("Missing or incorrect file format.");
+		}
+
 		try {
-			
-			//Required steps to load XML document. 
+
+			// Required steps to load XML document.
 			SAXBuilder sax = new SAXBuilder();
 			Document doc = sax.build(new File("./src/nz/ac/vuw/ecs/swen225/gp21/Persistency/levels/" + fileName));
-			
-			//Get the "Game" element. 
+
+			// Get the "Game" element.
 			Element root = doc.getRootElement();
-			
-			//Get the attributes from the "Game" element. 
+
+			// Get the attributes from the "Game" element.
 			int levelNumber = Integer.parseInt(root.getAttributeValue("levelNumber"));
 			int totalTime = Integer.parseInt(root.getAttributeValue("totalTime"));
 			int chapRow = Integer.parseInt(root.getAttributeValue("row"));
 			int chapCol = Integer.parseInt(root.getAttributeValue("col"));
 
-			//Get the parent element for all Tile elements.
+			// Get the parent element for all Tile elements.
 			Element tiles = root.getChild("Tiles");
 
-			//Calculate the rows and cols. We assume that the board is rectangular. 
+			// Calculate the rows and cols. We assume that the board is rectangular.
 			int totalRows = tiles.getChildren().size();
 			int totalCols = tiles.getChildren().get(0).getChildren().size();
-			
-			//Construct a 2D array using calculated dimensions.
+
+			System.out.println("totalRows: " + totalRows);
+			System.out.println("totalCols: " + totalCols);
+			// Construct a 2D array using calculated dimensions.
 			Tile[][] maze = new Tile[totalRows][totalCols];
 
-			//Loop through tiles element and construct tile objects using its children. 
+			// Loop through tiles element and construct tile objects using its children.
 			for (int row = 0; row < totalRows; row++) {
 				Element currentItem = tiles.getChildren().get(row);
 				for (int col = 0; col < totalCols; col++) {
 					Element currentTile = currentItem.getChildren().get(col);
-					
-					//Find the currentTiles type and construct different subtypes of Tile depending on that attribute.
-					String type = currentTile.getAttributeValue("type");
+
+					// Find the currentTiles type and construct different subtypes of Tile depending
+					// on that attribute.
+					String type = currentTile.getName();
 					if (type.equals("Free")) {
 						String key = currentTile.getAttributeValue("key");
 						boolean treasure = Boolean.parseBoolean(currentTile.getAttributeValue("treasure"));
@@ -77,6 +90,13 @@ public class XMLSaveLoad {
 					} else if (type.equals("Door")) {
 						String colour = currentTile.getAttributeValue("colour");
 						maze[row][col] = new Door(colour);
+					} else if (type.equals("Exit")) {
+						maze[row][col] = new Exit();
+					} else if (type.equals("ExitLock")) {
+						maze[row][col] = new ExitLock();
+					} else if (type.equals("Info")) {
+						String info = currentTile.getAttributeValue("info");
+						maze[row][col] = new Info(info);
 					}
 				}
 			}
@@ -87,67 +107,74 @@ public class XMLSaveLoad {
 			System.out.println("Error loading file." + e);
 		}
 
-		//Could not load file. Return null. 
+		// Could not load file. Return null.
 		return null;
 	}
 
 	/**
 	 * Saves the given game object to an XML file.
 	 * 
-	 * With help from: https://www.journaldev.com/1211/jdom-write-xml-file-example-object
+	 * With help from:
+	 * https://www.journaldev.com/1211/jdom-write-xml-file-example-object
 	 * 
-	 * @param game	The object to convert to XML.  
-	 * @param outputFileName 	The filename to save it to. (Should be in the format "file_name.xml")
+	 * @param game           The object to convert to XML.
+	 * @param outputFileName The filename to save it to. (Should be in the format
+	 *                       "file_name.xml")
+	 * @throws IOException If input file name is in the incorrect format.
 	 *
 	 */
-	public static void save(Game game, String outputFileName) {
-		
+	public static void save(Game game, String outputFileName) throws IOException {
+
+		if (!outputFileName.endsWith(".xml")) {
+			throw new IOException("Missing or incorrect file format.");
+		}
+
 		Document doc = new Document();
-		
-		//Create our root element and save it to a variable. 
+
+		// Create our root element and save it to a variable.
 		doc.setRootElement(new Element("Game"));
 		Element root = doc.getRootElement();
 
-		//Add the game parameters as attributes. 
+		// Add the game parameters as attributes.
 		root.setAttribute("levelNumber", "" + game.getLevel());
 		root.setAttribute("totalTime", "" + game.timeLeft());
 		root.setAttribute("row", "" + game.findChap().getRow());
 		root.setAttribute("col", "" + game.findChap().getCol());
 
-		//Create a parent element which will contain all child Tiles. 
+		// Create a parent element which will contain all child Tiles.
 		Element allTiles = new Element("Tiles");
 
 		Tile[][] maze = game.getMaze();
-		
-		//Loop through all indices of the maze:
+
+		// Loop through all indices of the maze:
 		for (int row = 0; row < maze.length; row++) {
-			//Get the current row of tiles. 
-			Element items = new Element("Items");
+			// Get the current row of tiles.
+			Element items = new Element("Row");
 			for (int col = 0; col < maze[row].length; col++) {
-				
+
 				Tile tileObject = maze[row][col];
-				
-				//Construct a tileElement to put all information from tileObject into. 
-				Element tileElement = new Element("Tile");
-				String tileClassName = tileObject.getClass().getSimpleName();
-				tileElement.setAttribute("type", tileClassName);
-				
-				//Save different attributes to tileElement depending on its type. 
+
+				// Construct a tileElement to put all information from tileObject into.
+				Element tileElement = new Element(tileObject.getClass().getSimpleName());
+
+				// Save different attributes to tileElement depending on its type.
 				if (tileObject instanceof Free) {
 					tileElement.setAttribute("key", ((Free) tileObject).getKey());
 					tileElement.setAttribute("treasure", "" + ((Free) tileObject).hasTreasure());
 				} else if (tileObject instanceof Door) {
 					tileElement.setAttribute("colour", ((Door) tileObject).getColour());
+				} else if (tileObject instanceof Info) {
+					tileElement.setAttribute("info", ((Info) tileObject).getInformation());
 				}
-				
-				//We can now add tileElement as a child of items. 
+
+				// We can now add tileElement as a child of items.
 				items.getChildren().add(tileElement);
 			}
-			//The row is complete. We can now add it as a child of allTiles. 
+			// The row is complete. We can now add it as a child of allTiles.
 			allTiles.getChildren().add(items);
 		}
-		
-		//We can now add the parent tile element as a child of root. 
+
+		// We can now add the parent tile element as a child of root.
 		root.getChildren().add(allTiles);
 
 		XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
@@ -155,23 +182,18 @@ public class XMLSaveLoad {
 			xmlOutputter.output(doc,
 					new FileOutputStream("./src/nz/ac/vuw/ecs/swen225/gp21/Persistency/levels/" + outputFileName));
 		} catch (IOException e) {
-			System.out.println("Error saving XML: " + e);
+			throw new IOException("Error saving XML: " + e);
 		}
 	}
 
 	public static void main(String args[]) {
 
-		load("level1.xml");
-
-		Tile[][] board = new Tile[2][2];
-		board[0][0] = new Free("", false);
-		board[1][0] = new Door("Green");
-		board[0][1] = new Wall();
-		board[1][1] = new Free("", false);
-
-		Game g = new Game(board, 60, 1, 1, 1);
-
-		save(g, "saved.xml");
+		try {
+			Game current = load("level1.xml");
+			System.out.println(current.drawBoard());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
