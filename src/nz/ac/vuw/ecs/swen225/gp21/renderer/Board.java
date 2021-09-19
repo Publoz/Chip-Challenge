4331295765e4ed04aec1126dc8a8ec8cf9ab9ac3
Cosmos Bar-Even <barevcosm@ecs.vuw.ac.nz>
2021-09-19 20:56:02 +1200

    Tiles draw according to Game class' maze tile types

--- src/nz/ac/vuw/ecs/swen225/gp21/renderer/Board.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/renderer/Board.java
@@ -1,27 +1,36 @@
 package nz.ac.vuw.ecs.swen225.gp21.renderer;
 
-import nz.ac.vuw.ecs.swen225.gp21.App.GUI;
 import nz.ac.vuw.ecs.swen225.gp21.App.MainFrame;
+import nz.ac.vuw.ecs.swen225.gp21.Persistency.XMLSaveLoad;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Game;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Position;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Tile;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Board {
     Graphics g;
     private JFrame mainFrame;
     private JComponent gameBoard;
-    private Tile[][] tiles = new Tile[9][9];
+    private ArrayList<RenderTile> renderTiles = new ArrayList();
+    private Tile[][] maze;
+    private Position chapPos;
+    private Game game;
+    public Board(Game game) {
+        this.game = game;
+        this.maze = game.getMaze();
 
-    public Board() {
-
+        /**
         for (int i = 0; i < tiles.length; i++) {
             for (int j = 0; j < tiles[i].length; j++) {
-                tiles[i][j] = new Tile(i, j);
+                tiles[i][j] = new RenderTile(i, j);
             }
         }
-
+        **/
+        //emulation code
         gameBoard = new JComponent() {
             protected void paintComponent(Graphics g) {
                 redraw(g);
@@ -36,26 +45,40 @@ public class Board {
         gameBoard.setBounds(boardBorder, boardBorder, gameBoardWidth - 2 * boardBorder, mainFrame.getHeight() - 3 * boardBorder);
         gameBoard.setBackground(new Color(190, 190, 190));
         gameBoard.setLayout(new GridLayout(9, 9));
-
         mainFrame.add(gameBoard);
         mainFrame.setVisible(true);
 
     }
-
-    public void redraw(Graphics g) {
-        for(Tile[] ts : tiles){
-            for(Tile t : ts){
-                t.draw(g);
-            }
-        }
-    }
-
+    //emulation code
     private int border(int width, int height) {
         int num = width<height? height : width;
         return (int)((0.05)*num);
     }
 
+    public void redraw(Graphics g) {
+        chapPos = game.getChap();
+        for (int row = 0; row < maze.length; row++) {
+            for (int col = 0; col < maze.length; col++) {
+                System.out.println("\""+maze[row][col].getKey()+"\"");
+                renderTiles.add( new RenderTile(row, col, maze[row][col].toString()));
+            }
+        }
+
+
+        for(RenderTile t : renderTiles){
+            System.out.println();
+            t.draw(g);
+        }
+    }
+
+
+
     public static void main(String[] args) throws FontFormatException, IOException {
-        new Board();
+        Game currentGame = XMLSaveLoad.load("level1.xml");
+        new Board(currentGame);
+    }
+
+    public JComponent getGameBoard(){
+        return this.gameBoard;
     }
 }
---
+++ src/nz/ac/vuw/ecs/swen225/gp21/renderer/RenderTile.java
@@ -0,0 +1,44 @@
+package nz.ac.vuw.ecs.swen225.gp21.renderer;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.IOException;
+
+
+public class RenderTile {
+    int x, y;
+    String imgType;
+    BufferedImage img = null;
+    public RenderTile(int xpos, int ypos, String imgType) {
+        this.imgType = imgType;
+        this.x = xpos;
+        this.y = ypos;
+
+
+        try {
+            if(imgType == null){
+                img = ImageIO.read(RenderTile.class.getResource("tile.png"));
+            } else if (imgType != null){
+                if(imgType == ""){
+                    img = ImageIO.read(RenderTile.class.getResource("key.png"));
+                } else {
+                    img = ImageIO.read(RenderTile.class.getResource(imgType + ".png"));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(imgType);
+        }
+
+    }
+
+    public void draw(Graphics g){
+        g.drawImage(img, this.x * 60, this.y * 60, 60, 60, new ImageObserver() {
+            @Override
+            public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
+                return false;
+            }
+        });
+    }
+}
