1f0dd24cadb3c4b0ffac1c570064b6343e53ec19
Cosmos <odosomsoc@gmail.com>
2021-10-01 15:02:50 +1300

    javadoc/checkstyle

--- src/nz/ac/vuw/ecs/swen225/gp21/renderer/Board.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/renderer/Board.java
@@ -1,21 +1,21 @@
 package nz.ac.vuw.ecs.swen225.gp21.renderer;
-import nz.ac.vuw.ecs.swen225.gp21.domain.Chap;
+
 import nz.ac.vuw.ecs.swen225.gp21.domain.Game;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Position;
-import nz.ac.vuw.ecs.swen225.gp21.domain.Tile;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.sound.sampled.*;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.List;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JComponent;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.Tile;
 
 public class Board {
     private JComponent gameBoard;
@@ -25,7 +25,17 @@ public class Board {
     private Game game;
     private Clip moveClip, pickupClip, doorClip, hurtClip, portalClip;
     AudioInputStream moveStream, pickupStream, doorStream, hurtStream, portalStream;
-    public static BufferedImage chap,empty,portal,lock,treasure,info,doorG,doorB,doorY,doorR,wall,keyY,keyG,keyB,keyR, acid, time, chapleft, chapright, chapup;
+    public static BufferedImage chap, empty, portal, lock, treasure, info, doorG, doorB, doorY, doorR, wall, keyY, keyG,
+            keyB, keyR, acid, time, chapleft, chapright, chapup;
+
+    /**
+     * Board is responsible for loading and storing resources used by RenderTiles.
+     * Board then constructs the display component for GUI using these resources.
+     *
+     * @author barevcosm
+     *
+     */
+
     public Board(Game game) {
         this.game = game;
         this.maze = game.getMaze();
@@ -34,7 +44,8 @@ public class Board {
         lChapPos = game.getChap();
         for (int row = 0; row < maze.length; row++) {
             for (int col = 0; col < maze[0].length; col++) {
-                    renderTiles[col][row] = (new RenderTile(col, row, maze[row][col].toString(), maze[row][col]));
+                renderTiles[col][row] = (new RenderTile(col, row,
+                        maze[row][col].toString(), maze[row][col]));
             }
         }
 
@@ -44,7 +55,7 @@ public class Board {
             }
         };
 
-        //preload all images.
+        // preload all images.
 
         try {
             empty = ImageIO.read(RenderTile.class.getResource("empty.png"));
@@ -68,8 +79,8 @@ public class Board {
             chapleft = ImageIO.read(RenderTile.class.getResource("chapleft.png"));
             chapright = ImageIO.read(RenderTile.class.getResource("chapright.png"));
             chapup = ImageIO.read(RenderTile.class.getResource("chapback.png"));
-            
-            //preload sounds
+
+            // preload sounds
             moveClip = AudioSystem.getClip();
             moveStream = AudioSystem.getAudioInputStream(RenderTile.class.getResource("move.wav"));
             moveClip.open(moveStream);
@@ -86,30 +97,39 @@ public class Board {
             portalStream = AudioSystem.getAudioInputStream(RenderTile.class.getResource("portal.wav"));
             portalClip.open(portalStream);
 
-
         } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
             System.out.println("Unable to load images");
         }
 
     }
+
+    /**
+     * Responsible for updating tile images on game update.
+     * Responsible for calling sound effects on move
+
+     * @param g instance
+     */
+
     public void redraw(Graphics g) {
         lChapPos = chapPos;
 
-        if(chapPos != game.getChap()) { chapPos = game.getChap(); }
-        if(lChapPos != chapPos){
+        if (chapPos != game.getChap()) {
+            chapPos = game.getChap();
+        }
+        if (lChapPos != chapPos) {
             playSound("move");
-            if(renderTiles[chapPos.getCol()][chapPos.getRow()].img == treasure
+            if (renderTiles[chapPos.getCol()][chapPos.getRow()].img == treasure
                     || renderTiles[chapPos.getCol()][chapPos.getRow()].img == keyG
                     || renderTiles[chapPos.getCol()][chapPos.getRow()].img == keyR
                     || renderTiles[chapPos.getCol()][chapPos.getRow()].img == keyB
-                    || renderTiles[chapPos.getCol()][chapPos.getRow()].img == keyY){
+                    || renderTiles[chapPos.getCol()][chapPos.getRow()].img == keyY) {
                 playSound("pickup");
             }
-            if(renderTiles[chapPos.getCol()][chapPos.getRow()].img == doorG
+            if (renderTiles[chapPos.getCol()][chapPos.getRow()].img == doorG
                     || renderTiles[chapPos.getCol()][chapPos.getRow()].img == doorB
                     || renderTiles[chapPos.getCol()][chapPos.getRow()].img == doorR
                     || renderTiles[chapPos.getCol()][chapPos.getRow()].img == doorY
-                    || renderTiles[chapPos.getCol()][chapPos.getRow()].img == lock){
+                    || renderTiles[chapPos.getCol()][chapPos.getRow()].img == lock) {
                 playSound("door");
             }
         }
@@ -117,10 +137,10 @@ public class Board {
         for (int row = 0; row < maze.length; row++) {
             for (int col = 0; col < maze[0].length; col++) {
                 renderTiles[col][row].tile = maze[row][col];
-                int xOffset = col - chapPos.getCol() + 4;
-                int yOffset = row - chapPos.getRow() + 4;
-                renderTiles[col][row].x = xOffset;
-                renderTiles[col][row].y = yOffset;
+                int xoffset = col - chapPos.getCol() + 4;
+                int yoffset = row - chapPos.getRow() + 4;
+                renderTiles[col][row].x = xoffset;
+                renderTiles[col][row].y = yoffset;
                 renderTiles[col][row].lasMoveDir = game.getLastMoveDir();
 
                 renderTiles[col][row].draw(g);
@@ -129,34 +149,44 @@ public class Board {
         }
     }
 
-    public void playSound(String s){
-      if(s.equals("move")){
-        moveClip.stop();
-        moveClip.setFramePosition(0);
-        moveClip.start();
-      } else if(s.equals("pickup")) {
-        pickupClip.stop();
-        pickupClip.setFramePosition(0);
-        pickupClip.start();
-      } else if(s.equals("door")) {
-        doorClip.stop();
-        doorClip.setFramePosition(0);
-        doorClip.start();
-      } else if(s.equals("portal")) {
-        portalClip.stop();
-        portalClip.setFramePosition(0);
-        portalClip.start();
-      } else if(s.equals("hurt")) {
-        hurtClip.stop();
-        hurtClip.setFramePosition(0);
-        hurtClip.start();
-      }
+    /**
+     * Plays sound file corresponding to string.
 
-      
+     * @param s sound file to be played
+     */
+
+    public void playSound(String s) {
+        if (s.equals("move")) {
+            moveClip.stop();
+            moveClip.setFramePosition(0);
+            moveClip.start();
+        } else if (s.equals("pickup")) {
+            pickupClip.stop();
+            pickupClip.setFramePosition(0);
+            pickupClip.start();
+        } else if (s.equals("door")) {
+            doorClip.stop();
+            doorClip.setFramePosition(0);
+            doorClip.start();
+        } else if (s.equals("portal")) {
+            portalClip.stop();
+            portalClip.setFramePosition(0);
+            portalClip.start();
+        } else if (s.equals("hurt")) {
+            hurtClip.stop();
+            hurtClip.setFramePosition(0);
+            hurtClip.start();
+        }
 
     }
 
-    public JComponent getGameBoard(){
+    /**
+     * Board Component.
+
+     * @return the game component used by GUI
+     */
+
+    public JComponent getGameBoard() {
         return this.gameBoard;
     }
 }
--- src/nz/ac/vuw/ecs/swen225/gp21/renderer/RenderTile.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/renderer/RenderTile.java
@@ -1,13 +1,23 @@
 package nz.ac.vuw.ecs.swen225.gp21.renderer;
 
-import nz.ac.vuw.ecs.swen225.gp21.domain.*;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.Tile;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Time;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Wall;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Acid;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Chap;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Door;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Exit;
+import nz.ac.vuw.ecs.swen225.gp21.domain.ExitLock;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Info;
+
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.IOException;
-
 
 public class RenderTile {
     int x, y;
@@ -16,6 +26,15 @@ public class RenderTile {
     public BufferedImage img;
     public String lasMoveDir;
     public boolean hasEnemy;
+
+    /**
+     * RenderTile is responsible for holding a image for a tile of the maze.
+     * Board is constructed using a grid of RenderTiles.
+     *
+     * @author barevcosm
+     *
+     */
+
     public RenderTile(int xpos, int ypos, String imgType, Tile t) {
         this.imgType = imgType;
         this.tile = t;
@@ -23,35 +42,80 @@ public class RenderTile {
         this.y = ypos;
     }
 
-    public void draw(Graphics g){
+    /**
+     * Chooses the correct image for a tile.
+     * Same tile types may use different images depending on state
+
+     * @param g instance
+     */
+
+    public void draw(Graphics g) {
         img = Board.empty;
-        if(tile != null){
-            if(tile instanceof Door){
-                if(((Door) tile).getColour().equals("g")){ img = Board.doorG; }
-                if(((Door) tile).getColour().equals("b")){ img = Board.doorB; }
-                if(((Door) tile).getColour().equals("y")){ img = Board.doorY; }
-                if(((Door) tile).getColour().equals("r")){ img = Board.doorR; }
+        if (tile != null) {
+            if (tile instanceof Door) {
+                if (((Door) tile).getColour().equals("g")) {
+                    img = Board.doorG;
+                }
+                if (((Door) tile).getColour().equals("b")) {
+                    img = Board.doorB;
+                }
+                if (((Door) tile).getColour().equals("y")) {
+                    img = Board.doorY;
+                }
+                if (((Door) tile).getColour().equals("r")) {
+                    img = Board.doorR;
+                }
             }
-            if(tile.hasTreasure()){img = Board.treasure;}
-            if(tile instanceof Wall){ img = Board.wall; }
-            if(tile instanceof ExitLock){ img = Board.lock; }
-            if(tile instanceof Exit){ img = Board.portal; }
-            if(tile instanceof Info) { img = Board.info;}
-            if(tile instanceof Time) {img = Board.time;}
-            if(tile instanceof Acid) {img = Board.acid;}
-            if(tile.hasKey()){
-                if(tile.getKey().equals("g")){ img = Board.keyG; }
-                if(tile.getKey().equals("b")){ img = Board.keyB; }
-                if(tile.getKey().equals("y")){ img = Board.keyY; }
-                if(tile.getKey().equals("r")){ img = Board.keyR; }
+            if (tile.hasTreasure()) {
+                img = Board.treasure;
             }
-            if(tile.getActor() instanceof Chap) {
+            if (tile instanceof Wall) {
+                img = Board.wall;
+            }
+            if (tile instanceof ExitLock) {
+                img = Board.lock;
+            }
+            if (tile instanceof Exit) {
+                img = Board.portal;
+            }
+            if (tile instanceof Info) {
+                img = Board.info;
+            }
+            if (tile instanceof Time) {
+                img = Board.time;
+            }
+            if (tile instanceof Acid) {
+                img = Board.acid;
+            }
+            if (tile.hasKey()) {
+                if (tile.getKey().equals("g")) {
+                    img = Board.keyG;
+                }
+                if (tile.getKey().equals("b")) {
+                    img = Board.keyB;
+                }
+                if (tile.getKey().equals("y")) {
+                    img = Board.keyY;
+                }
+                if (tile.getKey().equals("r")) {
+                    img = Board.keyR;
+                }
+            }
+            if (tile.getActor() instanceof Chap) {
                 img = Board.chap;
-                if(lasMoveDir == "w") {img = Board.chapup;}
-                if(lasMoveDir == "a") {img = Board.chapleft;}
-                if(lasMoveDir == "d") {img = Board.chapright;}
+                if (lasMoveDir == "w") {
+                    img = Board.chapup;
+                }
+                if (lasMoveDir == "a") {
+                    img = Board.chapleft;
+                }
+                if (lasMoveDir == "d") {
+                    img = Board.chapright;
+                }
             }
-            if(tile.getActor() != null && !(tile.getActor() instanceof Chap)){img = tile.getActor().getImage(); }
+            if (tile.getActor() != null && !(tile.getActor() instanceof Chap)) {
+                img = tile.getActor().getImage();
+            }
         }
 
         g.drawImage(img, this.x * 60, this.y * 60, 60, 60, new ImageObserver() {
